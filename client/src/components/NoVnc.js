ca402a93306fd5403be9b3eafe5b1ecee544b07a
Bob Whitelock <bob.whitelock@alces-software.com>
2016-01-18 14:16:23 +0000

    Make keyboard keys consistently be sent to session
    This seems to fix issue where keys not being sent to the VNC session, is
    a combination of what is done in the Angular equivalent code and
    information from https://github.com/kanaka/noVNC/issues/506. Still
    requires initial unfocus and refocus of noVNC canvas to work.

--- client/src/components/NoVnc.js
+++ client/src/components/NoVnc.js
@@ -1,7 +1,6 @@
 'use strict';
 
 import React from 'react';
-
 import noVNC from 'novnc-node';
 
 require('styles/NoVnc.scss');
@@ -9,17 +8,40 @@ require('styles/NoVnc.scss');
 class NoVnc extends React.Component {
   render() {
     return (
-      <canvas id='novnc-canvas'></canvas>
+      <canvas
+        id='novnc-canvas'
+        onMouseOver={this.handleMouseOver.bind(this)}
+        onMouseOut={this.handleMouseOut.bind(this)}
+        >
+      </canvas>
     );
   }
+
   componentDidMount() {
-    const rfb = new noVNC.RFB({
-      target: document.getElementById('novnc-canvas')
+    this.canvas = document.getElementById('novnc-canvas');
+    this.rfb = new noVNC.RFB({
+      local_cursor: true,
+      target: this.canvas,
     });
 
     // /websocket suffix required as specified here:
     // http://procbits.com/2013/10/09/connecting-to-a-sockjs-server-from-native-html5-websocket.
-    rfb.connect('ws://localhost:6080/websocket', 6080);
+    this.rfb.connect('ws://localhost:6080/websocket', 6080);
+  }
+
+  handleMouseOver() {
+    // When component moused over make keyboard send keys to canvas and focus
+    // the canvas, so keys sent to the VNC session; this does not seem to
+    // consistently happen otherwise.
+    // TODO: still need to unfocus and refocus canvas once for this to work at
+    // the moment.
+    this.rfb.get_keyboard().set_focused(true);
+    this.canvas.focus();
+  }
+
+  handleMouseOut() {
+    // Don't send keys to canvas any more when mouse leaves.
+    this.rfb.get_keyboard().set_focused(false);
   }
 }
 
