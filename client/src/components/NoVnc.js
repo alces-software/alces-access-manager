df9a6796ed2dbfd48237847408bd95cc2ba0ca13
Bob Whitelock <bob.whitelock@alces-software.com>
2016-01-19 19:15:41 +0000

    Add event creation code adapted from Portal
    As in Portal, added a state handler function which will create an event
    every time the things we care about happen in noVNC, we should then be
    able to add handlers for these to do useful stuff.
    
    Using jQuery for this at the moment; I'm not sure how good an idea this
    is mixing this in with React but seems OK currently and wasn't working
    without it (but may be able to get this working with more effort).
    
    Also add a button for connecting now, rather than doing this
    automatically, for easier testing.

--- client/src/components/NoVnc.js
+++ client/src/components/NoVnc.js
@@ -2,28 +2,63 @@
 
 import React from 'react';
 import noVNC from 'novnc-node';
+import $ from 'jquery';
 
 require('styles/NoVnc.scss');
 
 class NoVnc extends React.Component {
   render() {
     return (
-      <canvas
-        id='novnc-canvas'
-        onMouseOver={this.handleMouseOver.bind(this)}
-        onMouseOut={this.handleMouseOut.bind(this)}
-        >
-      </canvas>
+      <div>
+        <button onClick={this.connect.bind(this)}>Connect</button>
+        <canvas
+          id='novnc-canvas'
+          onMouseOver={this.handleMouseOver.bind(this)}
+          onMouseOut={this.handleMouseOut.bind(this)}
+          >
+        </canvas>
+      </div>
     );
   }
 
   componentDidMount() {
     this.canvas = document.getElementById('novnc-canvas');
+
+    // Add event handlers.
+    $(this.canvas).on('vnc:connect', this.handleVncConnect.bind(this));
+
     this.rfb = new noVNC.RFB({
       local_cursor: true,
       target: this.canvas,
+      onUpdateState: this.stateHandler.bind(this),
     });
+  }
 
+  stateHandler(rfb, state, oldstate, msg) {
+    // TODO: Rather than having custom events, it would be more React-y to
+    // dispatch actions and monitor the state.
+    const statesMap = {
+      loaded: 'load',
+      connect: 'start',
+      failed: 'failure',
+      fatal: 'fatal',
+      normal: 'connect',
+      disconnected: 'disconnect',
+    };
+    const eventState = statesMap[state];
+    if (eventState && this.canvas) {
+      const eventName = `vnc:${eventState}`
+      const data = {rfb, state, oldstate, msg,};
+      $(this.canvas).trigger(eventName, data);
+    }
+  }
+
+  componentWillUnmount() {
+    // TODO: Not actually manually tested yet as component never unmounts.
+    $(this.canvas).off();
+  }
+
+  connect() {
     // /websocket suffix required as specified here:
     // http://procbits.com/2013/10/09/connecting-to-a-sockjs-server-from-native-html5-websocket.
     this.rfb.connect('ws://localhost:6080/websocket', 6080);
@@ -42,6 +77,10 @@ class NoVnc extends React.Component {
   handleMouseOut() {
     // Don't send keys to canvas any more when mouse leaves.
     this.rfb.get_keyboard().set_focused(false);
+  }
+
+  handleVncConnect(event, data) {
+    console.log('handling', event, data);
   }
 }
 
