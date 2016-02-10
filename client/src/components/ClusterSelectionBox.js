7f495bc9508e114bc2c2b3b914a250fee05d2ca7
Bob Whitelock <bob.whitelock@alces-software.com>
2016-02-10 18:59:19 +0000

    Add rest of cluster login form and improve styling

--- client/src/components/ClusterSelectionBox.js
+++ client/src/components/ClusterSelectionBox.js
@@ -1,20 +1,34 @@
 
 import React from 'react';
-import {Input} from 'react-bootstrap';
+import {ButtonInput, Input} from 'react-bootstrap';
 import FlipCard from 'react-flipcard';
 
 export default class ClusterSelectionBox extends React.Component {
   render() {
     const {name} = this.props;
+
+    const nameElement = <div className="cluster-selection-box-name">{name}</div>;
     return (
       <div
         className="cluster-selection-box"
         >
         <FlipCard>
-          <div className="cluster-selection-box-front">{name}</div>
+          <div className="cluster-selection-box-front">
+            {nameElement}
+            <div>[Cluster details to go here]</div>
+          </div>
           <div className="cluster-selection-box-back">
-            <Input label="Username" type="text"/>
-            <Input label="Password" type="password"/>
+            {nameElement}
+            <form>
+              <Input placeholder="Username" type="text"/>
+              <Input placeholder="Password" type="password"/>
+              <ButtonInput
+                className="cluster-selection-box-submit"
+                type="submit"
+                value="View"
+                bsStyle="success"
+              />
+            </form>
           </div>
         </FlipCard>
       </div>
