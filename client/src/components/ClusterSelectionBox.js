145c0c0d32b4f1add1396bc520c6128fa224a673
Bob Whitelock <bob.whitelock@alces-software.com>
2016-02-10 18:20:33 +0000

    Remove custom focus/flip handling
    Actually seems to work well without this, can always look into improving
    this UI in more depth later.

--- client/src/components/ClusterSelectionBox.js
+++ client/src/components/ClusterSelectionBox.js
@@ -4,23 +4,13 @@ import {Input} from 'react-bootstrap';
 import FlipCard from 'react-flipcard';
 
 export default class ClusterSelectionBox extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { isFlipped: false };
-  }
-
   render() {
     const {name} = this.props;
     return (
       <div
         className="cluster-selection-box"
-        onBlur={this.handleBlur.bind(this)}
-        onFocus={this.handleFocus.bind(this)}
         >
-        <FlipCard
-          disabled={true}
-          flipped={this.state.isFlipped}
-          >
+        <FlipCard>
           <div className="cluster-selection-box-front">{name}</div>
           <div className="cluster-selection-box-back">
             <Input label="Username" type="text"/>
@@ -29,21 +19,5 @@ export default class ClusterSelectionBox extends React.Component {
         </FlipCard>
       </div>
     );
-  }
-
-  handleFocus() {
-    console.log(this.state.isFlipped); // eslint-disable-line no-console
-    if (!this.state.isFlipped) {
-      this.setFlipped(true);
-    }
-  }
-
-  handleBlur() {
-    this.setFlipped(false);
-  }
-
-  setFlipped(isFlipped) {
-    const newState = {isFlipped}
-    this.setState(newState);
   }
 }
