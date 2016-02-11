540c2480b259a7dfef1efad999f6b9718c4700d4
Bob Whitelock <bob.whitelock@alces-software.com>
2016-02-11 10:49:42 +0000

    Add environment box to cluster selection page

--- client/src/components/pages/ClusterSelectionPage.js
+++ client/src/components/pages/ClusterSelectionPage.js
@@ -1,20 +1,39 @@
 
 import React from 'react';
+import {Grid, Row, Col} from 'react-bootstrap';
 
 import ClusterSelectionBox from 'components/ClusterSelectionBox';
-import FlightGrid from 'components/FlightGrid';
 
 require("styles/ClusterSelectionPage.scss");
 
 export default class ClusterSelectionPage extends React.Component {
   render() {
     return (
-      <FlightGrid columns={3} containerClassName="cluster-selection-page">
-        <ClusterSelectionBox name="A normal cluster"></ClusterSelectionBox>
-        <ClusterSelectionBox name="Better cluster"></ClusterSelectionBox>
-        <ClusterSelectionBox name="Test cluster"></ClusterSelectionBox>
-        <ClusterSelectionBox name="The best cluster"></ClusterSelectionBox>
-      </FlightGrid>
+      <div className="container">
+        <Grid>
+          <Row>
+            <Col md={12}>
+              <div className="environment-details-box">You are connected to an environment. Select a cluster below to get started.</div>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={4}>
+              <ClusterSelectionBox name="A normal cluster"></ClusterSelectionBox>
+            </Col>
+            <Col md={4}>
+              <ClusterSelectionBox name="Better cluster"></ClusterSelectionBox>
+            </Col>
+            <Col md={4}>
+              <ClusterSelectionBox name="Test cluster"></ClusterSelectionBox>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={4}>
+              <ClusterSelectionBox name="Best cluster"></ClusterSelectionBox>
+            </Col>
+          </Row>
+        </Grid>
+      </div>
     );
   }
 }
