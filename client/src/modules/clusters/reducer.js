5f9449c5c5953c5379fe8ebb0fc340f3341a9351
Bob Whitelock <bob.whitelock@alces-software.com>
2016-02-11 13:23:49 +0000

    Add toggle for whether to use demo data or not

--- client/src/modules/clusters/reducer.js
+++ client/src/modules/clusters/reducer.js
@@ -1,13 +1,18 @@
 
 
 function initialState() {
-  return [
-    {id: 1, name: "A normal cluster"},
-    {id: 2, name: "Better cluster"},
-    {id: 3, name: "Test cluster"},
-    {id: 4, name: "Best cluster"},
-  ]
-
+  // TODO factor demo-checking out so done in central location
+  if (__DEMO__) {
+    return [
+      {id: 1, name: "A normal cluster"},
+      {id: 2, name: "Better cluster"},
+      {id: 3, name: "Test cluster"},
+      {id: 4, name: "Best cluster"},
+    ];
+  }
+  else {
+    return [];
+  }
 }
 
 export default function reducer(state=initialState(), action) {
