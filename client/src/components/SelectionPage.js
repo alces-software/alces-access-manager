3a8d303721f64743f1080439dbb4e0857a56ca82
Bob Whitelock <bob.whitelock@alces-software.com>
2016-02-12 10:52:14 +0000

    Factor out duplication of cluster/session pages

---
+++ client/src/components/SelectionPage.js
@@ -0,0 +1,53 @@
+
+import _ from 'lodash';
+import React from 'react';
+import {Grid, Row, Col} from 'react-bootstrap';
+
+require("styles/ClusterSelectionPage.scss");
+
+export default class ClusterSelectionPage extends React.Component {
+  render() {
+    const {headerText, items} = this.props;
+    const groupedItems = _.chunk(items, 3);
+
+    return (
+      <div className="container">
+        <Grid>
+          <Row>
+            <Col md={12}>
+              <div className="environment-details-box">
+                {headerText}
+              </div>
+            </Col>
+          </Row>
+          {this.renderRows(groupedItems)}
+        </Grid>
+      </div>
+    );
+  }
+
+  renderRows(rows) {
+    return _.map(rows, (row, key) => (
+      <Row key={key}>
+        {this.renderRow(row)}
+      </Row>
+    ));
+  }
+
+  renderRow(row) {
+    return _.map(row, (item) => this.renderColumn(item));
+  }
+
+  renderColumn(item) {
+    const selectionBoxElement = React.createElement(
+      this.props.selectionBoxComponent,
+      {name: item.name}
+    );
+
+    return (
+      <Col md={4} key={item.id}>
+        {selectionBoxElement}
+      </Col>
+    )
+  }
+}
--- client/src/components/pages/SessionSelectionPage.js
+++ client/src/components/pages/SessionSelectionPage.js
@@ -1,50 +1,27 @@
 
-import _ from 'lodash';
 import React from 'react';
-import {Grid, Row, Col} from 'react-bootstrap';
 
 import SessionSelectionBox from 'components/SessionSelectionBox';
+import SelectionPage from 'components/SelectionPage';
 
 // require("styles/ClusterSelectionPage.scss");
 
 export default class SessionSelectionPage extends React.Component {
   render() {
     const {cluster, sessions} = this.props;
-    const groupedSessions = _.chunk(sessions, 3);
+    const headerText = (
+      <span>
+        Viewing sessions on cluster <em>{cluster.name}</em>. Select a session
+        to connect to below.
+      </span>
+    )
 
     return (
-      <div className="container">
-        <Grid>
-          <Row>
-            <Col md={12}>
-              <div className="environment-details-box">
-                Viewing sessions on cluster <em>{cluster.name}</em>. Select a session to connect to below.
-              </div>
-            </Col>
-          </Row>
-          {this.renderClusterRows(groupedSessions)}
-        </Grid>
-      </div>
-    );
-  }
-
-  renderClusterRows(rows) {
-    return _.map(rows, (row, key) => (
-      <Row key={key}>
-        {this.renderClusterRow(row)}
-      </Row>
-    ));
-  }
-
-  renderClusterRow(row) {
-    return _.map(row, (cluster) => this.renderClusterColumn(cluster));
-  }
-
-  renderClusterColumn(cluster) {
-    return (
-      <Col md={4} key={cluster.id}>
-        <SessionSelectionBox name={cluster.name}/>
-      </Col>
+      <SelectionPage
+        items={sessions}
+        headerText={headerText}
+        selectionBoxComponent={SessionSelectionBox}
+      />
     )
   }
 }
