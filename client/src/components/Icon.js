9361e6ad35e96ca914609a0e24d38dd1c9689c6b
Bob Whitelock <bob.whitelock@alces-software.com>
2016-02-12 15:01:43 +0000

    Add refresh sessions button to sessions page
    Other definitions in Icon mapping (as taken from Flight) cleared, will
    need to adapt these later when factor this component out. Button also
    ugly at the moment, will improve look later.

--- client/src/components/Icon.js
+++ client/src/components/Icon.js
@@ -10,47 +10,7 @@ import FontAwesome from 'react-fontawesome';
 import classNames from 'classnames';
 
 const iconNameToFontAwesomeProps = {
-  /* Invitation icons */
-  /* Actions we can take on invitations and their feedback */
-  "invitation-accept":    {name: "check"},
-  "invitation-accepting": {name: "spinner", spin: true},
-  "invitation-decline":   {name: "times"},
-  "invitation-declining": {name: "spinner", spin: true},
-  /* States that an invitation can be in */
-  "invitation-pending":   {name: "chain-broken"},
-  "invitation-accepted":   {name: "chain"},
-  "invitation-declined":   {name: "ban"},
-
-  /* Cluster icons */
-  "clusters":            {name: "server"},
-  "cluster":             {name: "server"},
-  "cluster-pending":     {name: "hourglass-o", className: "animation-spinStopSpin"},
-  "cluster-terminating": {name: "hourglass-o", className: "animation-spinStopSpin"},
-  "cluster-running":     {name: "cog", className: "animation-slowSpin"},
-  "cluster-failed":      {name: "exclamation-circle"},
-  "cluster-ssh-access":  {name: "terminal"},
-  "cluster-reference":   {name: "sun-o"},
-  "cluster-launch":      {name: "rocket"},
-  "cluster-terminate":   {name: "stop"},
-  "cluster-composition": {name: "list-alt"},
-
-  /* Environment icons */
-  "environments":       {name: "bank"},
-  "environment":        {name: "bank"},
-  "environment-target": {name: "bullseye"},
-  "environment-join":   {name: "share-square"},
-  "environment-create": {name: "plus-square"},
-  "environment-share":  {name: "share-alt-square"},
-
-  /* Component icons */
-  "component": {name: "puzzle-piece"},
-  "component-create": {name: "plus-square"},
-
-  /* Miscellaneous icons */
-  "arrow-go":     {name: "arrow-right"},
-  "arrow-return": {name: "arrow-left"},
-  "connect":      {name: "bolt"},
-  "summary":      {name: "info-circle"}
+  "sessions-reload":    {name: "refresh"},
 }
 
 const faPropsForIconName = (iconName) => {
@@ -80,5 +40,5 @@ export default class Icon extends React.Component {
 Icon.propTypes = {
   ...FontAwesome.propTypes,
   name: PropTypes.string.isRequired,
-  className: PropTypes.string
+  className: PropTypes.string,
 };
--- client/src/components/pages/SessionSelectionPage.js
+++ client/src/components/pages/SessionSelectionPage.js
@@ -1,8 +1,10 @@
 
 import React from 'react';
+import {Button} from 'react-bootstrap';
 
 import SessionSelectionBox from 'components/SessionSelectionBox';
 import SelectionPage from 'components/SelectionPage';
+import Icon from 'components/Icon';
 
 // require("styles/ClusterSelectionPage.scss");
 
@@ -10,10 +12,22 @@ export default class SessionSelectionPage extends React.Component {
   render() {
     const {cluster, sessions} = this.props;
     const headerText = (
-      <span>
-        Viewing sessions on cluster <em>{cluster.name}</em>. Select a session
-        to connect to below.
-      </span>
+      <p>
+        <span>
+          Viewing sessions on cluster <em>{cluster.name}</em>. Select a session
+          to connect to below.
+        </span>
+        &nbsp;
+        <span>
+          <Button
+            bsStyle="primary"
+            className="sessions-reload-button"
+            >
+            Refresh sessions&nbsp;
+            <Icon name="sessions-reload" size="2x"/>
+          </Button>
+        </span>
+      </p>
     )
 
     return (
