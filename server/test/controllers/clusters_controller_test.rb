60fb11771ea50e78dd04a161df3bac4f57d84214
Bob Whitelock <bob.whitelock@alces-software.com>
2016-02-15 10:48:09 +0000

    Add clusters route; just returns parsed config

---
+++ server/test/controllers/clusters_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+require 'yaml'
+
+class ClustersControllerTest < ActionController::TestCase
+  test "should return the clusters specified in the config file" do
+    get :index
+    assert_response :success
+
+    sample_config_file = File.join(File.dirname(File.expand_path(__FILE__)), 'sample_config.yaml')
+    config = YAML.load_file(sample_config_file)
+    json_response = JSON.parse(@response.body)
+    assert json_response == config
+  end
+end
