dfa4465ae0f028edec49df35d926d51255fb301f
Bob Whitelock <bob.whitelock@alces-software.com>
2016-02-15 12:11:42 +0000

    Move specifying AM config file to Rails config
    Can then mock this in tests to test different configs.

--- server/app/controllers/api/v1/clusters_controller.rb
+++ server/app/controllers/api/v1/clusters_controller.rb
@@ -2,9 +2,13 @@ require 'yaml'
 
 class Api::V1::ClustersController < ApplicationController
   def index
-    config_file = File.join(File.dirname(File.expand_path(__FILE__)), '../../../../test/controllers/api/v1/sample_config.yaml')
     config = YAML.load_file(config_file)
-
     render json: config.to_json
+  end
+
+  private
+
+  def config_file
+    Rails.configuration.x.config_file
   end
 end
--- server/test/controllers/api/v1/clusters_controller_test.rb
+++ server/test/controllers/api/v1/clusters_controller_test.rb
@@ -1,14 +1,17 @@
-require 'test_helper'
 require 'yaml'
+
+require 'test_helper'
 
 class Api::V1::ClustersControllerTest < ActionController::TestCase
   test "should return the clusters specified in the config file" do
+    sample_config_file_path = File.join(File.dirname(File.expand_path(__FILE__)), 'sample_config.yaml')
+    @controller.stubs(:config_file).returns(sample_config_file_path)
+
     get :index
     assert_response :success
 
-    sample_config_file = File.join(File.dirname(File.expand_path(__FILE__)), 'sample_config.yaml')
-    config = YAML.load_file(sample_config_file)
+    config = YAML.load_file(sample_config_file_path)
     json_response = JSON.parse(@response.body)
-    assert json_response == config
+    assert_equal json_response, config
   end
 end
