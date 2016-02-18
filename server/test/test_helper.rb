75be0b42749a396f674d358eb77323967827a8a2
Bob Whitelock <bob.whitelock@alces-software.com>
2016-02-18 15:59:11 +0000

    Colorize test output

--- server/test/test_helper.rb
+++ server/test/test_helper.rb
@@ -3,6 +3,10 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'mocha/mini_test'
 
+require "minitest/reporters"
+Minitest::Reporters.use!
+
+
 class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
 end
