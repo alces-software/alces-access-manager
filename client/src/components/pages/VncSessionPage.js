e770c7fa119b9d9b5cfc9be32e50b358ad812c60
Bob Whitelock <bob.whitelock@alces-software.com>
2016-02-24 14:41:56 +0000

    Populate NoVnc component with current session info
    This means it is now possible to go from viewing all clusters to
    authenticating and connecting to a particular session on a cluster.

--- client/src/components/pages/VncSessionPage.js
+++ client/src/components/pages/VncSessionPage.js
@@ -6,11 +6,11 @@ import NoVnc from 'components/NoVnc';
 export default class VncSessionPage extends React.Component {
   render() {
     const {cluster, session} = this.props;
-    const url = `ws://${cluster.ip}:${session.port}/websockify`
+    const url = `ws://${cluster.ip}:${session.websocket}/websockify`
 
     return (
       <div className="container">
-        <NoVnc url={url} password="37Cqa92R"/>
+        <NoVnc url={url} password={session.password}/>
       </div>
     );
   }
