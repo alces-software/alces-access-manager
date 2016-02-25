6014dda8a9c601dd1e7c281fd9e0c077c6cff53b
Bob Whitelock <bob.whitelock@alces-software.com>
2016-02-25 12:25:36 +0000

    Rough version of different cluster boxes
    Very rough version of having different versions of cluster selection
    box depending on whether logged in to cluster or not, needs refactoring.

--- client/src/components/ClusterSelectionBox.js
+++ client/src/components/ClusterSelectionBox.js
@@ -4,6 +4,7 @@ import React from 'react';
 import {Button, Input} from 'react-bootstrap';
 import FlipCard from 'react-flipcard';
 import {reduxForm} from 'redux-form';
+import {Link} from 'react-router';
 
 import Icon from 'components/Icon';
 import {selectionBoxPropTypes} from 'utils/propTypes';
@@ -30,42 +31,78 @@ class ClusterSelectionBox extends React.Component {
       </span>
     );
 
-    return (
-      <div
-        className="cluster-selection-box"
-        >
-        <FlipCard>
-          <div className="cluster-selection-box-front">
-            <p>
-              {cluster.name}
-            </p>
-            <p>
-              IP: {cluster.ip}
-            </p>
-            <p>
-              [Cluster details to go here]
-            </p>
-          </div>
-          <div className="cluster-selection-box-back">
-            <p>
-              {cluster.name}
-            </p>
-            <form onSubmit={handleSubmit(authenticateCluster)}>
-              <Input placeholder="Username" type="text" {...username}/>
-              <Input placeholder="Password" type="password" {...password}/>
+    const authenticatedButtonText = (
+      <span>
+        View&nbsp;&nbsp;<Icon name="cluster-authenticate-submit"/>
+      </span>
+    );
+
+    const clusterLink = `/cluster/${cluster.ip}`;
+
+    const box = cluster.authenticated_username ?
+      (
+        <div
+          className="session-selection-box"
+          >
+          <p>
+            {cluster.name}
+          </p>
+          <p>
+            IP: {cluster.ip}
+          </p>
+          <p>
+            [Cluster details to go here]
+          </p>
+          <Link to={clusterLink}>
+            <Button
+              className="selection-box-button"
+              type="button"
+              bsStyle="success"
+              >
+              {authenticatedButtonText}
+            </Button>
+          </Link>
+        </div>
+    )
+    :
+      (
+        <div
+          className="cluster-selection-box"
+          >
+          <FlipCard>
+            <div className="cluster-selection-box-front">
+              <p>
+                {cluster.name}
+              </p>
+              <p>
+                IP: {cluster.ip}
+              </p>
+              <p>
+                [Cluster details to go here]
+              </p>
+            </div>
+            <div className="cluster-selection-box-back">
+              <p>
+                {cluster.name}
+              </p>
+              <form onSubmit={handleSubmit(authenticateCluster)}>
+                <Input placeholder="Username" type="text" {...username}/>
+                <Input placeholder="Password" type="password" {...password}/>
                 <Button
                   className="selection-box-button"
                   type="submit"
                   bsStyle="success"
                   disabled={incomplete || submitting}
-                >
+                  >
                   {submitButtonText}
                 </Button>
-            </form>
-          </div>
-        </FlipCard>
-      </div>
-    );
+              </form>
+            </div>
+          </FlipCard>
+        </div>
+    )
+
+    return box;
   }
 }
 
