b6a2d3bac69b5b2e34973d6b64eba2c0f42355fa
Bob Whitelock <bob.whitelock@alces-software.com>
2016-02-29 17:10:01 +0000

    Make session reload animation less jarring
    By always showing spinner for at least a second.

--- client/src/modules/ui/reducer.js
+++ client/src/modules/ui/reducer.js
@@ -1,6 +1,7 @@
 
 import {resolve, reject} from 'redux-simple-promise';
 
+import * as actionTypes from './actionTypes';
 import * as clusterActionTypes from 'clusters/actionTypes';
 import * as sessionActionTypes from 'sessions/actionTypes';
 
@@ -18,8 +19,7 @@ export default function reducer(state=initialState, action) {
     case sessionActionTypes.LOAD_SESSIONS:
       return {...state, loadingSessions: true}
 
-    case resolve(sessionActionTypes.LOAD_SESSIONS):
-    case reject(sessionActionTypes.LOAD_SESSIONS):
+    case actionTypes.STOP_SESSION_RELOAD_ANIMATION:
       return {...state, loadingSessions: false}
 
     default:
