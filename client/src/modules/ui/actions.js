5fc1e0cd50d7a844ee1ec7041ef67e90be1c718e
Bob Whitelock <bob.whitelock@alces-software.com>
2016-03-01 12:31:56 +0000

    Load sessions for authed clusters on app load
    Rather than only doing this when auth with a cluster or click session
    reload button.

--- client/src/modules/ui/actions.js
+++ client/src/modules/ui/actions.js
@@ -1,8 +1,26 @@
 
+import _ from 'lodash';
+
 import * as actionTypes from './actionTypes';
+import {loadClusters} from 'clusters/actions';
+import {loadSessions} from 'sessions/actions';
 
 export function stopSessionReloadAnimation() {
   return {
     type: actionTypes.STOP_SESSION_RELOAD_ANIMATION,
+  }
+}
+
+export function loadSessionData() {
+  return (dispatch) => {
+    dispatch(loadClusters()).
+      then( (result) => {
+        const clusters = result.clusters;
+        _.map(clusters, (cluster) => {
+          if (cluster.authenticated_username) {
+            dispatch(loadSessions(cluster.ip));
+          }
+        });
+    });
   }
 }
