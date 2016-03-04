aeac01214b0af72172c80b26930049bc60eca87f
Bob Whitelock <bob.whitelock@alces-software.com>
2016-03-04 15:32:39 +0000

    Display modal messages when copy fails
    When no text selected or copied yet within the VNC session, or the
    copying fails for some unknown reason, display an appropriate modal.

--- client/src/components/pages/VncSessionPage.js
+++ client/src/components/pages/VncSessionPage.js
@@ -3,10 +3,47 @@ import ClipboardAction from 'clipboard/lib/clipboard-action';
 import React from 'react';
 import {ButtonGroup, ButtonToolbar} from 'react-bootstrap';
 
+import {ContactCustomerSupport} from 'components/CustomerSupport';
 import NoVnc from 'components/NoVnc';
 import ToolbarButton from 'components/ToolbarButton';
+import {infoGeneratorsMap} from "notification/messageGeneration"
+import MessageGenerator from "notification/MessageGenerator";
+
+// Set up copy messages.
+const noTextCode = 'vnc-session-no-text';
+const copyFailedCode = 'vnc-session-copy-failed';
+infoGeneratorsMap.addGeneratorForCode(
+  noTextCode,
+  new MessageGenerator(
+    "No text to copy",
+    <p>
+      Select or copy some text within the VNC session to copy to your
+      computer's clipboard.
+    </p>
+  )
+).addGeneratorForCode(
+  copyFailedCode,
+  new MessageGenerator(
+    "Copy failed",
+    <p>
+      The copy of text from the VNC session failed, possibly because your web
+      browser does not support this feature. <ContactCustomerSupport/>
+    </p>
+  )
+);
 
 export default class VncSessionPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.emitter = {
+      emit: (event) => {
+        if (event !== 'success') {
+          this.props.displayInfoModal(copyFailedCode);
+        }
+      },
+    }
+  }
+
   render() {
     const {cluster, novnc, session, stateChange, setCopyText} = this.props;
 
@@ -60,13 +97,19 @@ export default class VncSessionPage extends React.Component {
   }
 
   handleClickCopyButton() {
-    const {novnc: {copyText}} = this.props;
+    const {novnc: {copyText}, displayInfoModal} = this.props;
+
+    // Display modal and do nothing if no text copied within session yet.
+    if (!copyText) {
+      displayInfoModal(noTextCode);
+      return;
+    }
 
     // This will copy the stored text, received from the session clipboard to
     // the system clipboard.
     new ClipboardAction({
       action: 'copy',
-      emitter: {emit: (event, payload) => console.log('Emit', event, payload)}, // eslint-disable-line no-console
+      emitter: this.emitter,
       text: copyText,
     });
   }
--- client/src/containers/VncSessionPageContainer.js
+++ client/src/containers/VncSessionPageContainer.js
@@ -1,10 +1,17 @@
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 import VncSessionPage from 'components/pages/VncSessionPage';
 import {vncSessionPageSelector} from 'selectors';
+import * as notificationActions from 'notification/actions';
 import * as novncActions from 'novnc/actions';
+
+const mapDispatchToProps = (dispatch) => ({
+  ...bindActionCreators(notificationActions, dispatch),
+  ...bindActionCreators(novncActions, dispatch),
+});
 
 export default connect(
   vncSessionPageSelector,
-  novncActions
+  mapDispatchToProps
 )(VncSessionPage);
