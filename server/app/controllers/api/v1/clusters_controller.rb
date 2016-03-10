d26b0917f532c1c3b557071a160733ed2754f942
Bob Whitelock <bob.whitelock@alces-software.com>
2016-03-10 12:04:46 +0000

    Make ssl daemon communication work
    Adapted from the similar functionality in ASM; if `ssl` is set to `true`
    for a cluster's config the config values in `ssl_connection` will be
    used for communicating with the cluster daemon.

--- server/app/controllers/api/v1/clusters_controller.rb
+++ server/app/controllers/api/v1/clusters_controller.rb
@@ -1,5 +1,7 @@
 require 'yaml'
 
+require 'alces/tools/ssl_configurator'
+
 class Api::V1::ClustersController < ApplicationController
   rescue_from DaemonClient::ConnError, with: :daemon_connection_error_handler
 
@@ -77,8 +79,44 @@ class Api::V1::ClustersController < ApplicationController
 
   def connection_opts
     cluster_daemon_address = "#{params[:ip]}:#{cluster_config[:auth_port]}"
-    cluster_config.slice(:ssl, :timeout).merge(address: cluster_daemon_address)
+    {
+      address: cluster_daemon_address,
+      timeout: cluster_config[:timeout],
+      ssl_config: cluster_config[:ssl] ? ssl_config : nil
+    }
   end
+
+  def ssl_config
+    # Adapted from similar code in ASM `config/initializers/config.rb`.
+    Class.new do
+      include Alces::Tools::SSLConfigurator
+
+      def initialize(cluster_config)
+        # Make the outer controller's cluster config available within this
+        # class.
+        @cluster_config = cluster_config
+      end
+
+      def ssl_verify_mode
+        if @cluster_config[:ssl_connection][:verify] == false
+          OpenSSL::SSL::VERIFY_NONE
+        else
+          OpenSSL::SSL::VERIFY_PEER | OpenSSL::SSL::VERIFY_FAIL_IF_NO_PEER_CERT
+        end
+      end
+
+      def ssl
+        ssl_opts = @cluster_config[:ssl_connection].dup
+        Alces::Tools::SSLConfigurator::Configuration.new(
+          root: ssl_opts[:root],
+          certificate: ssl_opts[:certificate],
+          key: ssl_opts[:key],
+          ca: ssl_opts[:ca]
+        )
+      end
+    end.new(cluster_config).ssl_config
+  end
+
 
   def sessions_for_response(username)
     opts = {
