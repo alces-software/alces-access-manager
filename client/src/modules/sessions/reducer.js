c5cf9971bd8825e99a0b74e440a06013f19fe459
Bob Whitelock <bob.whitelock@alces-software.com>
2016-03-29 17:32:44 +0100

    Only update sessions when launch succeeds
    The updated sessions are only included when the launch succeeds, as the
    sessions will most likely not have changed if the launch fails, so we
    should only update the sessions on successful launch.

--- client/src/modules/sessions/reducer.js
+++ client/src/modules/sessions/reducer.js
@@ -3,19 +3,28 @@ import {resolve} from 'redux-simple-promise';
 
 import * as actionTypes from './actionTypes';
 
+function handleReceivedSessions(state, action) {
+  const clusterIp = action.meta.payload.cluster.ip;
+  const sessions = action.payload.sessions;
+  return {
+    ...state,
+    [clusterIp]: sessions,
+  }
+}
+
 const initialState = {};
 export default function reducer(state=initialState, action) {
   switch (action.type) {
 
     case resolve(actionTypes.LOAD_SESSIONS):
     case resolve(actionTypes.RELOAD_SESSIONS):
+      return handleReceivedSessions(state, action);
+
     case resolve(actionTypes.LAUNCH):
-      const clusterIp = action.meta.payload.cluster.ip;
-      const sessions = action.payload.sessions;
-      return {
-        ...state,
-        [clusterIp]: sessions,
+      if (action.payload.success) {
+        return handleReceivedSessions(state, action);
       }
+      return state;
 
     default:
       return state;
