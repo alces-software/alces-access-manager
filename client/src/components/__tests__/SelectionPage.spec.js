d2287ddabf8ecce71aa96a8848bca9b56693490d
Bob Whitelock <bob.whitelock@alces-software.com>
2016-04-06 15:52:44 +0100

    Only update SelectionPage when items are different

--- client/src/components/__tests__/SelectionPage.spec.js
+++ client/src/components/__tests__/SelectionPage.spec.js
@@ -9,26 +9,39 @@ import SelectionPage from 'components/SelectionPage';
 describe('SelectionPage', function() {
   describe('shouldComponentupdate', function() {
     beforeEach(function() {
-      const props = {
+      this.props = {
         header: <div/>,
         items: [],
         keyProp: 'id',
         selectionBoxComponent: 'div',
         loggingOut: false,
       }
-      this.selectionPage = renderIntoDocument(<SelectionPage {...props}/>);
+
+      this.selectionPage =
+        () => renderIntoDocument(<SelectionPage {...this.props}/>);
     })
 
     it ("doesn't update when logging out", function() {
       const nextProps = {loggingOut: true};
-      const shouldUpdate = this.selectionPage.shouldComponentUpdate(nextProps);
+      const shouldUpdate = this.selectionPage().loggingOutShouldUpdate(nextProps);
       expect(shouldUpdate).to.be.false;
     });
 
     it ('updates when not logging out', function() {
       const nextProps = {loggingOut: false};
-      const shouldUpdate = this.selectionPage.shouldComponentUpdate(nextProps);
+      const shouldUpdate = this.selectionPage().loggingOutShouldUpdate(nextProps);
       expect(shouldUpdate).to.be.true;
+    });
+
+    it ("only updates when items are different", function() {
+      const items = [{id: 1}, {id: 2}, {id: 3}];
+      this.props = {...this.props, items};
+
+      const selectionPage = this.selectionPage();
+      expect(selectionPage.itemsDifferentShouldUpdate({items})).to.be.false;
+
+      const newItems = [{id: 4}];
+      expect(selectionPage.itemsDifferentShouldUpdate({items: newItems})).to.be.true;
     });
   });
 });
