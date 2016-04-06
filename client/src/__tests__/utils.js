d8998e24b1a4b5e7ab27d9c653fd0503ecb80db9
Bob Whitelock <bob.whitelock@alces-software.com>
2016-04-06 13:15:16 +0100

    Changes to code from Avaitor to make tests run

--- client/src/__tests__/utils.js
+++ client/src/__tests__/utils.js
@@ -1,19 +1,11 @@
 /* Useful testing routines */
 
-import * as formNames from 'forms/names';
-
 export const emptyStore = {
   getState() {
     return {
-      auth: {signingIn: false, signingOut: false},
-      clusters: {data: {}, meta: {}},
-      clusterComponents: {data: {}, meta: {}},
-      environments: {data: {}, meta: {}},
-      valetConfigurations: {data: {}, meta: {}},
-      form: {[formNames.CLUSTER_COMPONENT_NEW]: {}}
     };
   },
 
   subscribe() {},
-  dispatch() {}
+  dispatch() {},
 }
